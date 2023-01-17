49821d7a4d04a5ca3ddcaa480d3e2754fb253fac
Eriuden <71089756+Eriuden@users.noreply.github.com>
2023-01-17 21:37:12 +0100

    Update db.js

--- api/config/db.js
+++ api/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
 .connect(
-    "mongodb+srv://"
-    + process.env.DB_USER_PASS +
-    "@cluster0.iodcc.mongodb.net/GenDate"
+    
 )
     .then(() => console.log("connecté à mongoDB"))
     .catch((err) => console.log("echec de connection à mongoDB",err))
