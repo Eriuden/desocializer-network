f99dec58ad4d56d7303b1d0514e2a1af73d6a870
Eriuden <antoinecarrette@hotmail.fr>
2023-08-18 18:05:34 +0200

    env update

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 mongoose
 .connect(
-    
+    "mongodb+srv://" + 
+    process.env.DB_USER_PASS +
+    "@cluster0.iodcc.mongodb.net/TheDesocializerNetwork"
 )
     .then(() => console.log("connecté à mongoDB"))
     .catch((err) => console.log("echec de connection à mongoDB",err))
